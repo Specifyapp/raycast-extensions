ad1c07c61d2d3f601e02a06a821872dc8163f044
Yann-Edern Gillet <helloyannglt@gmail.com>
2022-12-29 16:28:09 +0100

    (feat): add documentation command

---
+++ extensions/specify/src/documentation/index.ts
@@ -0,0 +1,102 @@
+export default {
+  "Overview": [
+    {
+      url: "https://docs.specifyapp.com/introduction",
+      title: "Introduction",
+    },
+    {
+      url: "https://docs.specifyapp.com/concepts",
+      title: "Concepts",
+    },
+    {
+      url: "https://docs.specifyapp.com/get-started",
+      title: "Get Started",
+    },
+  ],
+  "Ecosystem": [
+    {
+      url: "https://docs.specifyapp.com/token-types",
+      title: "Token types",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#border",
+      title: "Border",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#bitmap",
+      title: "Bitmap",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#color",
+      title: "Color",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#depth",
+      title: "Depth",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#duration",
+      title: "Duration",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#font",
+      title: "Font",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#gradient",
+      title: "Gradient",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#measurement",
+      title: "Measurement",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#opacity",
+      title: "Opacity",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#shadow",
+      title: "Shadow",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#style",
+      title: "Text Style",
+    },
+    {
+      url: "https://docs.specifyapp.com/token-types#vector",
+      title: "Vector",
+    },
+    {
+      url: "https://docs.specifyapp.com/parsers",
+      title: "Parsers",
+    },
+    {
+      url: "https://docs.specifyapp.com/rules",
+      title: "Rules",
+    },
+  ],
+  "For Developers": [
+    {
+      url: "https://docs.specifyapp.com/cli",
+      title: "CLI",
+    },
+    {
+      url: "https://docs.specifyapp.com/rest-api",
+      title: "REST API",
+    },
+  ],
+  "Other Resources": [
+    {
+      url: "https://specifyapp.com/guides/design-data-platforms-101/",
+      title: "Design Data Platforms 101 Guide",
+    },
+    {
+      url: "https://github.com/Specifyapp/parsers",
+      title: "Parsers GitHub Repository",
+    },
+    {
+      url: "https://specifyapp.com/templates",
+      title: "Templates Gallery",
+    },
+  ],
+};
